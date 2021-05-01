73e5f9979a4c89ef7d65160031513049a9a193e5
cottongit0 <cottongit0@gmail.com>
2021-05-01 20:38:47 +0900

    이름 입력
    입력, 로컬 저장

--- JavaScript/user.js
+++ JavaScript/user.js
@@ -5,3 +5,21 @@ const userInput = userSection.querySelector(".form-user-name input");
 const greeting = document.querySelector(".greeting");
 
 const USER_NAME = "CURRENT_USER";
+
+userForm.addEventListener("submit", () => {
+  const currentValue = userInput.value;
+  localStorage.setItem(USER_NAME, currentValue);
+});
+
+function greetingUser() {
+  const currentUser = localStorage.getItem(USER_NAME);
+  if (USER_NAME === null) {
+    greeting.innerHTML = "유저 이름이 입력되지 않았습니다.";
+  } else {
+    greeting.innerHTML = `안녕하세요. ${currentUser}님`;
+  }
+}
+
+function renewUser() {
+  const renewUser = document.querySelector();
+}
