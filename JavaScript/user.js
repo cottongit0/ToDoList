a05dc11286640cf252c0488585140207d21b2eb7
cottongit0 <cottongit0@gmail.com>
2021-05-01 21:37:35 +0900

    유저 프로필
    로컬 저장, 갱신 시 텍스트 변경

--- JavaScript/user.js
+++ JavaScript/user.js
@@ -6,20 +6,37 @@ const greeting = document.querySelector(".greeting");
 
 const USER_NAME = "CURRENT_USER";
 
+// 유저 팝업 열고 닫기
+document.querySelector(".user").addEventListener("click", () => {
+  userSection.style.display = "flex";
+});
+userSection.querySelector(".close").addEventListener("click", () => {
+  userSection.style.display = "none";
+});
+
+// 유저 이름 입력
 userForm.addEventListener("submit", () => {
   const currentValue = userInput.value;
   localStorage.setItem(USER_NAME, currentValue);
+  greetingUser(currentValue);
 });
 
+// 인사 출력
 function greetingUser() {
   const currentUser = localStorage.getItem(USER_NAME);
-  if (USER_NAME === null) {
+  if (currentUser === null) {
     greeting.innerHTML = "유저 이름이 입력되지 않았습니다.";
   } else {
     greeting.innerHTML = `안녕하세요. ${currentUser}님`;
+    renewUserName();
   }
 }
 
-function renewUser() {
-  const renewUser = document.querySelector();
+function renewUserName() {
+  const infoDescription = document.querySelector("#user .container h2");
+  infoDescription.innerHTML = "유저이름을 바꾸시겠습니까?";
 }
+
+(function init() {
+  greetingUser();
+})();
